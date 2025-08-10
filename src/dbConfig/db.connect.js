31c7796c2e05fcae6662cdcc8d861d7eede84c91
EK GAUTHAM <sggtm42@gmail.com>
2025-08-10 20:11:21 +0700

    expamle deployment

--- src/dbConfig/db.connect.js
+++ src/dbConfig/db.connect.js
@@ -1,19 +1,17 @@
-import mongoose from "mongoose";
-
 const connection = { isConnected: false };
 
-export async function connectToDb()  {
-    if(connection.isConnected) {
-        console.log("Already connect to db")
-        return
-    }
-    try {
-        const connectDb = await mongoose.connect(process.env.MONGO_URI)
-        connection.isConnected = connectDb.connections[0].readyState
-        console.log('Db is connected')
-    } catch (error) {
-        console.log(error)
-        process.exit(1)
-    }
-    
+export async function connectToDb() {
+  if (connection.isConnected) {
+    console.log("Already connected to db");
+    return;
+  }
+  try {
+    const mongoose = (await import("mongoose")).default;
+    const connectDb = await mongoose.connect(process.env.MONGO_URI);
+    connection.isConnected = connectDb.connections[0].readyState;
+    console.log("Db is connected");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 }
