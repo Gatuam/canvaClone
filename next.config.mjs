8cad2b88ef0987affbaed955a84d57c95f4dc491
EK GAUTHAM <sggtm42@gmail.com>
2025-08-12 18:46:56 +0700

    deployment eror

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,16 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      mongoose: require.resolve('mongoose'),
+    };
+    return config;
+  },
+  experimental: {
+    esmExternals: false,
+  },
 };
 
 export default nextConfig;
