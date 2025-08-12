411495dc2a3009e5141b8ed60e4dd81e2198df3f
EK GAUTHAM <sggtm42@gmail.com>
2025-08-12 18:33:03 +0700

    mongoose fixed

--- src/dbConfig/db.connect.js
+++ src/dbConfig/db.connect.js
@@ -1,17 +1,20 @@
-const connection = { isConnected: false };
+import mongoose from "mongoose";
 
-export async function connectToDb() {
-  if (connection.isConnected) {
-    console.log("Already connected to db");
-    return;
-  }
-  try {
-    const mongoose = (await import("mongoose")).default;
-    const connectDb = await mongoose.connect(process.env.MONGO_URI);
-    connection.isConnected = connectDb.connections[0].readyState;
-    console.log("Db is connected");
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+const connection  = {}
+
+async function connectToDb()  {
+    if(connection.isConnected) {
+        console.log("Already connect to db")
+        return
+    }
+    try {
+        const connectDb = await mongoose.connect(process.env.MONGO_URI || '')
+        connection.isConnected = connectDb.connections[0].readyState
+        console.log('Db is connected')
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+    
 }
+export default connectToDb;
